6bf6bb3ccdd0c9e7a6aaa60cb25e29fd71539aa4
Ubuntu <ubuntu@ip-172-31-5-43.ap-southeast-2.compute.internal>
2025-06-07 20:18:29 +0000

    ec2 startup script improved.

--- ec2_spot.sh
+++ ec2_spot.sh
@@ -3,9 +3,14 @@
 
 
 sudo apt update
-sudo apt install libepoxy-dev libglm-dev
+sudo apt install -y libepoxy-dev libglm-dev nvtop
 
 python3 -m venv venv
 source venv/bin/activate
 python -m pip install --upgrade pip
 pip install -r ./requirements.txt
+
+mkdir build
+cd build
+cmake ../src/
+make -j5 all
