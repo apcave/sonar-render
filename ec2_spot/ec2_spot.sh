3bec6007242b6a99a4464fc608817e49dff98e43
Dr Alexander P Cave <alex.cave@outlook.com.au>
2025-06-09 22:11:05 +1000

    Express setup with ec2.

--- ec2_spot/ec2_spot.sh
+++ ec2_spot/ec2_spot.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-
+mkdir -p ~/.aws
+mv config ~/.aws/config
+mv credentials ~/.aws/credentials
 
 sudo apt update
 sudo apt install -y libepoxy-dev libglm-dev nvtop
+
+git clone https://github.com/apcave/sonar-render.git
+cd sonar-render
 
 python3 -m venv venv
 source venv/bin/activate
